29aa00dcb66dc20dcbd58af0d14c70bcf5a87dd9
bill-hirsh <neomath@gmail.com>
2024-11-15 02:11:16 -0600

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+def word_count(file):
+    count = 0
+    current_words = []
+
+    try:
+        for line in file.splitlines():
+            if (line.find("*** START") == -1):
+                continue 
+            elif (line.isspace() == True):
+                continue
+            elif (line.find("*** END") == -1):
+                pass
+            else:
+                break
+
+
+    except Exception as e:
+        print(e)
+
+
+    return count
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         
         print(file_contents)
+        print(f"There are {word_count(file_contents)} words in the book")
 
 main()
