57766234b5f90735a1c9a9510a4b8a555d5748eb
bill-hirsh <neomath@gmail.com>
2024-11-15 20:30:56 -0600

    add number count to main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+def letter_count(file):
+    letter_dict = {}
+
+    try:
+        for line in file:
+            if (line.isspace() == True):
+                continue
+            else:
+                for letter in line:
+                    if(letter.isalpha() == True):
+                        letter = letter.lower()
+                        x = letter_dict.get(letter, 0)
+                        letter_dict[letter] = x + 1
+                    else:
+                        continue
+
+    except Exception as e:
+        print(e)
+
+    return letter_dict
+
 def word_count(file):
     count = 0
 
@@ -8,7 +29,6 @@ def word_count(file):
             else:
 
                 words = line.split(" ")
-                print(words)
                 for word in words:
                         if (word == ""):
                             continue
@@ -25,5 +45,8 @@ def main():
         file_contents = f.readlines()
 
         print(f"There are {word_count(file_contents)} words in the book")
+        
+        for letter, count in letter_count(file_contents).items():
+            print(f"The {letter} character was found {count} times")
 
 main()
